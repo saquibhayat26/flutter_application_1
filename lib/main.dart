c755856341abb45ccc26a4b1fd8f3430061d6090
mshayat <saquibhayat26@gmail.com>
2022-12-01 12:27:59 +0530

    scaffold, drawer and buttons created second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,76 @@
-// import 'package:flutter/material.dart';
-
-// import 'screens/home_screen.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.green,
-//       ),
-//       home: const HomePage(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: "My Awesome App",
-    home: Container(color: Color.fromRGBO(100, 0, 0, 1)),
+    home: HomePage(),
   ));
 }
 
-// import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-// void main() => runApp(const MyApp());
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Material App',
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Material App Bar'),
-//         ),
-//         body: const Center(
-//           child: Text('Hello World'),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Awesome App"),
+        backgroundColor: Colors.red,
+      ),
+      body: const Text("I am body"),
+      drawer: Drawer(
+        width: 260,
+        // elevation: ,
+        child: ListView(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text("Saquib Hayat"),
+              accountEmail: Text("mshayatdevop@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("images/usersqb.jpg"),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+            // ignore: prefer_const_constructors
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text("Tile1"),
+              subtitle: const Text("STile1"),
+              trailing: const Icon(Icons.edit),
+            ),
+            // ignore: prefer_const_constructors
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text("Tile2"),
+              subtitle: const Text("STile2"),
+              trailing: const Icon(Icons.edit),
+            ),
+            // ignore: prefer_const_constructors
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text("Tile3"),
+              subtitle: const Text("STile3"),
+              trailing: const Icon(Icons.edit),
+            ),
+            const AboutListTile(
+              icon: Icon(Icons.info),
+              applicationIcon: Icon(Icons.local_play),
+              applicationName: "Awesome App",
+              applicationVersion: "1.0.25",
+              applicationLegalese: "© 2022 Company",
+              aboutBoxChildren: [Text("Developed with Love from MsHayat")],
+              child: Text("About my app"),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          backgroundColor: Colors.red,
+          child: const Icon(Icons.edit)),
+    );
+  }
+}
